940c8d3f38dfad5677cf8efa8642524ad99977bc
Danchiwaz <mainadanielchiwaz@gmail.com>
2023-06-07 09:57:19 +0300

    first commit

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+    reducer:{}
+});
+
+
+
+// notes
+// the store acts the overall state of the application 
